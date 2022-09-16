46aa277cbdb27d30a39cc51bd889be25f39d5cfb
nurbol <nuk_48@yahoo.com>
2022-09-16 20:28:10 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -7,21 +7,20 @@ var scores = [0, 0];
 // Тоглогчийн ээлжин дээр цуглуулж байгаа хувьсагч
 var roundScore = 0;
 
-// Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-var dice = Math.floor(Math.random() * 6) + 1;
-
-/* <div class="player-score" id="score-0">43</div> */
-// window.document.querySelector("#score-0").textContent = dice;
-
-// document.querySelector("#score-1").textContent = dice;
-
 // Программ эхлэхэд бэлтэх
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
+var diceDOM = document.querySelector(".dice");
 
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector(".dice").style.display = "none";
+diceDOM.style.display = "none";
 
-console.log("Dice : " + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDOM.src = "dice-" + diceNumber + ".png";
+  diceDOM.style.display = "block";
+});
