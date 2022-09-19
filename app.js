8e5226d975886c57ff8d9a471ab7fd6aee56208b
nurbol <nuk_48@yahoo.com>
2022-09-19 18:14:02 +0800

    2 shootoi huvilbariig buren ajilladag bolgoloo

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var isNewGame;
 
 // Программ эхлэхэд бэлтэх
 var activePlayer, scores, roundScore;
-var diceDOM = document.querySelector(".dice");
+
+var dice1 = document.querySelector(".dice1");
+var dice2 = document.querySelector(".dice2");
 
 ResetGame();
 
@@ -26,28 +28,44 @@ function ResetGame() {
   document.getElementById("current-0").textContent = 0;
   document.getElementById("current-1").textContent = 0;
 
-  diceDOM.style.display = "none";
+  dice1.style.display = "none";
+  dice2.style.display = "none";
 
   document.querySelector(".btn-roll").style.visibility = "visible";
   document.querySelector(".btn-hold").style.visibility = "visible";
+}
+
+function ChangePlayer() {
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = 0;
+  dice1.style.display = "none";
+  dice2.style.display = "none";
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
 }
 
 //Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
   if (isNewGame) {
     // Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // var diceNumber = Math.floor(Math.random() * 6) + 1;
+    var diceNumber1 = Math.floor(Math.random() * 6) + 1;
+    var diceNumber2 = Math.floor(Math.random() * 6) + 1;
 
     // Буусан шоонд харгалзах зургийг харуулах
-    diceDOM.src = "dice-" + diceNumber + ".png";
+    // diceDOM.src = "dice-" + diceNumber + ".png";
+    dice1.src = "dice-" + diceNumber1 + ".png";
+    dice2.src = "dice-" + diceNumber2 + ".png";
 
     // Шооны зургийг шаргаж ирэх
-    diceDOM.style.display = "block";
+    dice1.style.display = "block";
+    dice2.style.display = "block";
 
     // Буусан тоо нь 1-ээс ялгаатай бол идэвхитэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ
-    if (diceNumber !== 1) {
+    if (diceNumber1 !== 1 && diceNumber2 !== 1) {
       // Буусан тоог тоглогчид нэмж өгнө
-      roundScore = roundScore + diceNumber;
+      roundScore = roundScore + diceNumber1 + diceNumber2;
       document.getElementById("current-" + activePlayer).textContent =
         roundScore;
     } else {
@@ -67,7 +85,8 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     if (scores[activePlayer] >= 100) {
       isNewGame = false;
 
-      diceDOM.style.display = "none";
+      dice1.style.display = "none";
+      dice2.style.display = "none";
 
       roundScore = 0;
       document.getElementById("current-" + activePlayer).textContent =
@@ -83,8 +102,8 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
 
       document.getElementById("name-" + activePlayer).textContent = "WINNER!";
 
-      // document.querySelector(".btn-roll").style.visibility = "hidden";
-      // document.querySelector(".btn-hold").style.visibility = "hidden";
+      document.querySelector(".btn-roll").style.visibility = "hidden";
+      document.querySelector(".btn-hold").style.visibility = "hidden";
     } else {
       ChangePlayer();
     }
@@ -92,14 +111,5 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     alert("Game is Over!");
   }
 });
-
-function ChangePlayer() {
-  roundScore = 0;
-  document.getElementById("current-" + activePlayer).textContent = 0;
-  diceDOM.style.display = "none";
-  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-  document.querySelector(".player-0-panel").classList.toggle("active");
-  document.querySelector(".player-1-panel").classList.toggle("active");
-}
 
 document.querySelector(".btn-new").addEventListener("click", ResetGame);
